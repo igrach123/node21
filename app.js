1de0e47204dd4efda2cef9e12b6cffaf49edb5f4
Igor <brajkovich.igor@gmail.com>
2021-02-02 12:09:04 +0100

    cc

--- app.js
+++ app.js
@@ -2,25 +2,24 @@ const express = require('express');
 //exrepss app
 const app = express();
 
+//register viewengine
+app.set('view engine', 'ejs');
+
 //listen for requests
 app.listen(3000);
 
 app.get('/', (req, res) => {
 
-    res.sendFile('./views/index.html', {root: __dirname})
+  res.render('index');
 });
 app.get('/about', (req, res) => {
 
-    res.sendFile('./views/about.html', {root: __dirname})
+    res.render('about');
 });
 
-//redirect 
-app.get('/about-us', (req, res) => {
-    res.redirect('/about');
-});
 
 //404 page
 app.use( (req, res) => {
-    res.status(404).sendFile('./views/404.html', {root: __dirname});
+    res.status(404).render('404');
 
 });
