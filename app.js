f9b0a443f4d364b89fd88838d0e38848141181b1
Igor <brajkovich.igor@gmail.com>
2021-02-03 11:23:29 +0100

    working css

--- app.js
+++ app.js
@@ -4,25 +4,33 @@ const app = express();
 
 //register viewengine
 app.set('view engine', 'ejs');
-
+//add css
+app.use(express.static(__dirname + '/public'));
 //listen for requests
 app.listen(3000);
 
 app.get('/', (req, res) => {
 
-  res.render('index');
+    const blogs = [
+        {title :'Igor is playing dota' , snippet: 'atus eveniet ratione temporibus aperiam harum alias officiis' },
+        {title :'Igor is playing Pubg' , snippet: 'atus eveniet ratione temporibus aperiam harum alias officiis' },
+        {title :'Igor is playing Warzone' , snippet: 'atus eveniet ratione temporibus aperiam harum alias officiis' }
+    ];
+
+    res.render('index', { title:'Home', blogs});
 });
+
 app.get('/about', (req, res) => {
 
-    res.render('about');
+    res.render('about', { title:'About' });
 });
 
 app.get('/blogs/create', (req , res) => {
-    res.render()
+    res.render('create',{ title:'Create a new blog' });
 });
 
 //404 page
 app.use( (req, res) => {
-    res.status(404).render('404');
+    res.status(404).render('404', { title:'404' });
 
 });
